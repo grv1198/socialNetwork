e236fbc444cb15f3df573911baa88a6d0b140c65
grv1198 <grv1198@gmail.com>
2018-11-16 19:59:47 +0530

    get & post api to get and create a user respectively

--- src/main/java/com/grv/restproject/socialNetwork/controller/UserResource.java
+++ src/main/java/com/grv/restproject/socialNetwork/controller/UserResource.java
@@ -1,6 +1,11 @@
 package com.grv.restproject.socialNetwork.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.grv.restproject.socialNetwork.user.service.User;
+import com.grv.restproject.socialNetwork.user.service.UserDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by gaurav on 14/11/18.
@@ -9,8 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserResource {
 
+    @Autowired
+    private UserDaoService userDaoService;
 
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
 
+        return userDaoService.getAllUsers();
+    }
 
+    @GetMapping("/users/{userId}")
+    public User getUsers(@PathVariable Integer userId){
 
+        return userDaoService.getUserById(userId);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        userDaoService.addUser(user);
+    }
 }
