58560f0a0a797fb8d41a84ccf257029af6be46e1
grv1198 <grv1198@gmail.com>
2019-01-12 12:36:59 +0530

    validation

--- src/main/java/com/grv/restproject/socialNetwork/user/service/User.java
+++ src/main/java/com/grv/restproject/socialNetwork/user/service/User.java
@@ -1,5 +1,7 @@
 package com.grv.restproject.socialNetwork.user.service;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 /**
@@ -10,8 +12,10 @@ public class User {
 
     private Integer id;
 
+    @Size(min = 2, message = "Name should be at least 2 characters long")
     private String name;
 
+    @Past
     private Date dob;
 
 
