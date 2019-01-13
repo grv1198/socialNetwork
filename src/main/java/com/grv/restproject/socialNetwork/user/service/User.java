8ac928eae65691adfa1146211667260251ef5a92
grv1198 <grv1198@gmail.com>
2019-01-13 11:04:16 +0530

    Swagger impl

--- src/main/java/com/grv/restproject/socialNetwork/user/service/User.java
+++ src/main/java/com/grv/restproject/socialNetwork/user/service/User.java
@@ -1,5 +1,8 @@
 package com.grv.restproject.socialNetwork.user.service;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -7,15 +10,18 @@ import java.util.Date;
 /**
  * Created by gaurav on 15/11/18.
  */
+@ApiModel(description = "This is a member of social network")
 public class User {
 
 
     private Integer id;
 
     @Size(min = 2, message = "Name should be at least 2 characters long")
+    @ApiModelProperty(notes = "Name should be at least 2 characters long")
     private String name;
 
-    @Past
+    @Past(message = "Birthdate should be in past.")
+    @ApiModelProperty(notes = "Birthdate should be in past.")
     private Date dob;
 
 
